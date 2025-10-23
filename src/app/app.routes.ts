a6c024cc92df114107919900a868b5e4f2622adc
Shafqat Haider <93528873+ShafqatHaider@users.noreply.github.com>
2025-10-23 22:30:42 +0500

    delete doubling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { BlogListComponent } from './public/blog-list/blog-list.component';
 import { BlogDetailsComponent } from './public/blog-details/blog-details.component';
-import { LoginComponent } from './damin/login/login.component';
-import { AdminDashboardComponent } from './damin/admin-dashboard/admin-dashboard.component';
 import { authGaurd } from './core/guards/auth.guard';
+import { LoginComponent } from './admin/login/login.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
 
