7abd387f5d80eab4317de8c5e0311b5f75ff88f1
Shafqat Haider <93528873+ShafqatHaider@users.noreply.github.com>
2025-10-23 21:55:23 +0500

    n

---
+++ src/app/core/services/blog.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { Blog } from "../models/blog.model";
+
+@Injectable({providedIn:'root'})
+
+export class BlogService{
+    private storageKey ='blogs';
+
+    getAll():Blog[]{
+        return JSON.parse(localStorage.getItem(this.storageKey) ||'[]');
+    }
+
+    getBysId(id:number):Blog | undefined {
+        return this.getAll().find(b=>b.id===id);
+    }
+
+    save(blog:Blog):void{
+        const blogs = this.getAll();
+        if(!blog.id){
+            blog.id = new Date().getTime();
+            blogs.push(blog)
+        }else{
+            const idx = blogs.findIndex(b=>b.id===blog.id);
+            blogs[idx]=blog
+
+        }
+
+        localStorage.setItem(this.storageKey, JSON.stringify(blogs));
+
+    }
+
+    delete(id:number):void{
+        const blogs = this.getAll().filter(b=>b.id!==id);
+        localStorage.setItem(this.storageKey, JSON.stringify(blogs));
+    }
+}
